14c5ceaaaec14120b233e2a8ecb6cc51e0146f6f
Matt Millican <matt@mattmillican.com>
2017-07-30 16:51:16 -0500

    Start work on navigating between manifest locations

--- app/pages/manifests/manifest-location.component.ts
+++ app/pages/manifests/manifest-location.component.ts
@@ -12,6 +12,9 @@ import { ManifestService } from "../../shared/manifest/manifest.service";
     providers: [ ManifestService ]
 })
 export class ManifestLocationComponent implements OnInit {
+    private _trainId = "";
+    private _locId = "";
+    
     private _manifest = new Manifest(null);
     private _manifestLocation = new ManifestLocation(null, null);
     
@@ -26,25 +29,22 @@ export class ManifestLocationComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        let trainId = "";
-        let locId = "";
-
         this._pageRoute.activatedRoute
             .switchMap((activatedRoute) => activatedRoute.params)
             .forEach((params) => {
-                trainId = params["trainId"];
-                locId = params["locId"];
+                this._trainId = params["trainId"];
+                this._locId = params["locId"];
             });
 
-        console.log('view manifest for train id ' + trainId + ' at loc ' + locId);
+        console.log('view manifest for train id ' + this._trainId + ' at loc ' + this._locId);
 
         // TODO: Get data
 
-        this._manifestService.getTrainManifest(trainId)
+        this._manifestService.getTrainManifest(this._trainId)
             .subscribe(data => {
                 this._manifest = data;
 
-                this._manifestLocation = this._manifest.manifestLocations.filter(ml => ml.id == locId)[0];
+                this._manifestLocation = this._manifest.manifestLocations.filter(ml => ml.id == this._locId)[0];
             });
     }
 
@@ -54,5 +54,22 @@ export class ManifestLocationComponent implements OnInit {
 
     get manifestLocation(): ManifestLocation {
         return this._manifestLocation;
+    }
+
+    public onGoToNextLocation() {
+        var nextLoc = this.getNextLocation();
+        
+        if (nextLoc) {
+            console.log('next loc ' + nextLoc);
+            this._routerExtensions.navigate(["/manifest", this._trainId, "loc", nextLoc.id]);
+        }
+    }
+
+    private getNextLocation():ManifestLocation {
+        let curLocIdx = this._manifest.manifestLocations.indexOf(this._manifestLocation);
+        let nextLocIdx = curLocIdx + 1;
+
+        let nextLoc = this._manifest.manifestLocations[nextLocIdx];
+        return nextLoc;
     }
 }
